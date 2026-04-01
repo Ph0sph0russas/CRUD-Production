24ee32bbbaa8c5bcaf53f787372e35d3f4607e71
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-04-01 22:13:28 +0800

    Пункт 4 сделан

---
+++ controllers/SearchController.php
@@ -0,0 +1,29 @@
+<?php
+require_once "BaseGearsTwigController.php";
+
+class SearchController extends BaseGearsTwigController{
+    public $template="search.twig";
+
+    public function getContext(): array
+    {
+        $context = parent::getContext();
+        $type=isset($_GET['type']) ? $_GET['type'] : '';
+        $title=isset($_GET['title']) ? $_GET['title'] : '';
+        $full_description=isset($_GET['full_description']) ? $_GET['full_description'] : '';
+        $sql = <<<EOL
+SELECT id, title
+FROM extreme_gears
+WHERE (:title = '' OR title like CONCAT('%', :title, '%'))
+    and ((type = :type) OR :type='все')
+    and (:full_description='' OR info like CONCAT('%', :full_description, '%'))
+EOL;
+    $query=$this->pdo->prepare($sql);
+    $query->bindValue("title", $title);
+    $query->bindValue("type", $type);
+    $query->bindValue("full_description",$full_description);
+    $query->execute();
+    
+    $context['objects']=$query->fetchAll();
+    return $context;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
     require_once '../framework/autoload.php';
     require_once "../controllers/MainController.php";
     require_once "../controllers/ObjectController.php";
+    require_once "../controllers/SearchController.php";
     require_once "../controllers/Controller404.php";
     $loader = new \Twig\Loader\FilesystemLoader('../views');
     $twig = new \Twig\Environment($loader,[
@@ -16,7 +17,7 @@
     $router = new Router($twig, $pdo);
     $router->add("/", MainController::class);
     $router->add("/extreme_gears/(?P<id>\d+)", ObjectController::class);
-    $router->add("/extreme_gears/(?P<id>\d+)/image", ObjectController::class); 
-    $router->add("/extreme_gears/(?P<id>\d+)/info", ObjectController::class);
+    $router->add("/search", SearchController::class);
+
     
     $router->get_or_default(Controller404::class);
