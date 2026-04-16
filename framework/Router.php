c876bef4bc2602584205476a37ca7effef0661c7
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-04-17 02:32:56 +0800

    Задание 6.1 сделано

--- framework/Router.php
+++ framework/Router.php
@@ -49,7 +49,7 @@ class Router {
         $url = $_SERVER["REQUEST_URI"]; 
         $path = parse_url($url, PHP_URL_PATH);
         $controller = $default_controller;
-$newRoute = null;
+        $newRoute = null;
 
         $matches=[]; 
         foreach($this->routes as $route) {
--- middlewares/LoginRequiredMiddleware.php
+++ middlewares/LoginRequiredMiddleware.php
@@ -4,19 +4,28 @@ class LoginRequiredMiddleware extends BaseMiddleware {
     public function apply(BaseController $controller, array $context)
     {
         
-        $valid_user = "user";
-        $valid_password = "12345";
         
-        
-        $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
+        $query=$controller->pdo->prepare("SELECT username, password from users");
+        $query->execute();
+        $context['users'] = $query->fetchAll();
+
+        $username = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
         $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
-        
-        
-        if ($valid_user != $user || $valid_password != $password) {
-            
+
+        $is_correct_user=0;
+        foreach($context['users'] as $valid_user)
+        {
+            if ($valid_user['username']==$username && $valid_user['password'] == $password)
+            {
+                $is_correct_user=1;
+            }  
+        }  
+        if ($is_correct_user==0)
+        {
             header('WWW-Authenticate: Basic realm="Space objects"');
             http_response_code(401);
             exit;
         }
+              
     }
 }
